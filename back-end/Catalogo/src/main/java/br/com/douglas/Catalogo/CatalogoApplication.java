f0f69a60709bff7d2878675dd83d3d1669314938
Douglas Santos <douglas_j.santos@hotmail.com>
2024-07-18 12:42:36 -0300

    commit inicial

---
+++ back-end/Catalogo/src/main/java/br/com/douglas/Catalogo/CatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglas.Catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+}
---
+++ back-end/Catalogo/src/test/java/br/com/douglas/Catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglas.Catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
